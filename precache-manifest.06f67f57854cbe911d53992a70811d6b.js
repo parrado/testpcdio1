0ef95d3c53601ec9f722a0faf4af0ad5d7463f7d
parrado <parrado@uniquindio.edu.co>
2019-08-22 14:17:36 -0500

    Updates

---
+++ precache-manifest.06f67f57854cbe911d53992a70811d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d6e6a94bdcd423b3afb1d0bd02c197",
+    "url": "/testpcdio1/index.html"
+  },
+  {
+    "revision": "c15056992a548dbbcf63",
+    "url": "/testpcdio1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1e1df3f1ba1325b1a839",
+    "url": "/testpcdio1/static/js/2.1f1b9e97.chunk.js"
+  },
+  {
+    "revision": "c15056992a548dbbcf63",
+    "url": "/testpcdio1/static/js/main.02dee519.chunk.js"
+  },
+  {
+    "revision": "a484bae5ee282b3212d9",
+    "url": "/testpcdio1/static/js/runtime~main.dfcf00da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testpcdio1/static/media/logo.5d5d9eef.svg"
+  }
+]);
